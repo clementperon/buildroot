082a304829ca685713147d16a8d4b74730e043c8
Clément Péron <peron.clem@gmail.com>
2020-03-29 11:02:20 +0200

    boot: add Allwinner EL2 barebone
    Signed-off-by: Clément Péron <peron.clem@gmail.com>

---
+++ boot/aw-el2-barebone/aw-el2-barebone.mk
@@ -0,0 +1,29 @@
+################################################################################
+#
+# aw-el2-barebone
+#
+################################################################################
+
+AW_EL2_BAREBONE_VERSION = 80eabe21acc997f59b000d0e3f53dbcf4f09cedf
+AW_EL2_BAREBONE_SITE = $(call github,Icenowy,aw-el2-barebone,$(AW_EL2_BAREBONE_VERSION))
+AW_EL2_BAREBONE_LICENSE = GPLv2
+AW_EL2_BAREBONE_LICENSE_FILES = LICENSE
+AW_EL2_BAREBONE_INSTALL_IMAGES = YES
+
+AW_EL2_BAREBONE_MAKE_ENV = CROSS_COMPILE=$(TARGET_CROSS)
+AW_EL2_BAREBONE_IMG_DIR = $(@D)
+
+ifeq ($(BR2_TARGET_AW_EL2_BAREBONE_DEBUG),y)
+AW_EL2_BAREBONE_MAKE_ENV += DEBUG=1
+endif
+
+define AW_EL2_BAREBONE_BUILD_CMDS
+	$(TARGET_MAKE_ENV) $(AW_EL2_BAREBONE_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define AW_EL2_BAREBONE_INSTALL_IMAGES_CMDS
+        $(INSTALL) -m 0644 $(AW_EL2_BAREBONE_IMG_DIR)/el2-bb.bin \
+                $(BINARIES_DIR)/hyp.bin
+endef
+
+$(eval $(generic-package))
